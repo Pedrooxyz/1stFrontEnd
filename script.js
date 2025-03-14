4b5f5d8d2f7cf4d584c9eb5e61096c45bda74af3
Pedro Ribeiro <151522723+Pedrooxyz@users.noreply.github.com>
2025-03-14 14:25:52 +0000

    Atualização inicial
    Primeira edição com o progresso de código realizado até ao momento na aula. Primeiros conceitos de HTML, CSS E JavaScript

---
+++ script.js
@@ -0,0 +1,13 @@
+const btn = document.querySelector('.btn-open');
+const form = document.querySelector('.fact-form');
+
+btnOpen.addEventListener('click', function() {
+    if(form.classList.contains('.hidden')){
+        form.classList.remove('.hidden');
+        btnOpen.textContent = 'Close';
+    }
+    else{
+        form.classList.add('hidden');
+        btnOpen.textContent = 'Share a fact';
+    }
+} )
