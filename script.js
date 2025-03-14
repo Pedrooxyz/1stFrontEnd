acfa8d2c9aab188ce436575c8b1aa921b734324a
CarolinaMB-22 <carolina2005mb@gmail.com>
2025-03-14 14:38:58 +0000

    Alterações
    Alterações no ficheiro .html e .js

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 const btnOpen = document.querySelector('.btn-open');
-const form = document.querySelector('.fact-form');
+const form = document.querySelector('.fact-form')
+
 
 btnOpen.addEventListener('click', function() {
-    if(form.classList.contains('.hidden')){
-        form.classList.remove('.hidden');
-        btnOpen.textContent = 'Close';
-    }
-    else{
+    if(form.classList.contains('hidden')){
+        form.classList.remove('hidden');
+        btnOpen.textContent='Close';
+    }else{
         form.classList.add('hidden');
-        btnOpen.textContent = 'Share a fact';
+        btnOpen.textContent='Share a fact';
     }
-} )
+})
